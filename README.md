8e3c14ceefc74efaae83b32756a88d7ee7c5c233
Alexander Lubovenko <lubovenko@me.com>
2023-08-17 12:35:52 -0300

    just cleaned up the code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UFOfileZipper
- 
+`tdUFOfileZipper.py`
+
+The script runs after saving the file and packs it into a zip archive. The previous version of the zip archive is copied with the "bek" suffix. In this way, you always have the current saved version of the ufo file in the zip archive and the second previous copy.
--- tdUFOfileZipper.py
+++ tdUFOfileZipper.py
@@ -2,6 +2,11 @@ from mojo.events import addObserver
 import os
 import shutil
 
+__doc__ = """
+The script runs after saving the file and packs it into a zip archive. The previous version of the zip archive is copied with the "bek" suffix. In this way, you always have the current saved version of the ufo file in the zip archive and the second previous copy.
+"""
+
+
 class UFOFileZipper(object):
 	def __init__(self):		
 		addObserver(self, "zippUFO", "fontDidSave")
